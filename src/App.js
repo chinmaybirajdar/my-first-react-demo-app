5ad0a0fc7a1bf9a7b74d8edf97595866c601bfcb
chinmaybirajdar <2019meb1251@iitrpr.ac.in>
2022-06-02 17:18:34 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello There I'm Chinmay</h1>
+      <h2>Nice To meet you here on my brand new web app.</h2>
+    </div>
+  );
+}
+
+export default App;
