2bc2ececdce197330cba47256351fb5976e12627
chinmaybirajdar <2019meb1251@iitrpr.ac.in>
2022-06-02 18:04:46 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello There I'm Chinmay</h1>
       <h2>Nice To meet you here on my brand new web app.</h2>
+      <p>You forgot to write about yourself</p>
     </div>
   );
 }
